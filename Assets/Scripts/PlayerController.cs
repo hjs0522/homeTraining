49de6479727f60e6e6f1a8bbc8d22fe7cd68eba4
dpel99959 <dpel99959@gmail.com>
2022-04-16 18:01:53 +0900

    implement movement bar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float m_moveSpeed = 10.0f;
+    public float m_curMovement;
     private float x, z;
-    void Start()
+
+    public MovementGaugeController m_movementGaugeCtlr;
+
+    private void Start()
     {
-        
+        m_curMovement = 0.0f;
+        m_movementGaugeCtlr.SetGaugeZero();
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PlusMovement(10.0f);
+        }
         z = Input.GetAxis("Vertical") * m_moveSpeed * Time.deltaTime;
         x = Input.GetAxis("Horizontal") * m_moveSpeed * Time.deltaTime;
         transform.Translate(x, 0 ,z);
+    }
+
+    void PlusMovement(float movement)
+    {
+        m_curMovement += movement;
+        m_movementGaugeCtlr.SetMovementGauge(m_curMovement);
     }
 }
